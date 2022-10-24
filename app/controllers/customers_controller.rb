f1954eb93977ad7b61e579a2c15ae66972c920a5
Ling Fung Chan <justinlfchan@gmail.com>
2022-10-24 22:10:56 +0100

    updated some upcases for class method

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -4,13 +4,13 @@ require_relative '../views/customers_view'
 class CustomersController
   def initialize(customer_repository)
     @customer_repository = customer_repository
-    @customers_view = customersView.new
+    @customers_view = CustomersView.new
   end
 
   def add
     name = @customers_view.ask_user_for(:name)
     address = @customers_view.ask_user_for(:address)
-    customer = customer.new(name: name, address: address)
+    customer = Customer.new(name: name, address: address)
     @customer_repository.create(customer)
     display_customers
   end
