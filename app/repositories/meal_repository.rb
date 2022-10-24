b4afd1161ca624cbfe9dde31df9f99c56323a5d9
Ling Fung Chan <justinlfchan@gmail.com>
2022-10-24 21:01:36 +0100

    completed meal repository

---
+++ app/repositories/meal_repository.rb
@@ -0,0 +1,47 @@
+require 'csv'
+require_relative '../models/meal.rb'
+
+class MealRepository
+  # attr_accessor :meals, :next_id
+
+  def initialize(csv_file)
+    @csv_file = csv_file
+    @meals = []
+    @next_id = 1
+    load_csv if File.exist?(csv_file)
+  end
+
+  def create(meal)
+    meal.id = @next_id
+    meals << meal
+    @next_id += 1
+    save_csv
+  end
+
+  def all
+    @meals
+  end
+
+  def find(id)
+    @meals.find { |meal| meal.id == id }
+  end
+
+  private
+
+  def load_csv
+    CSV.foreach(@csv_file, headers: first_row, header_converters: :symbol) do |row|
+      row[:id] = row[:id].to_i
+      row[:price] = row[:price].to_i
+      @meals << Meal.new(row)
+    end
+    @next_id = @meals.empty? ? 1 : @meals.last.id + 1
+  end
+
+  def save_csv
+    CSV.open(@csv_file, "wb") do |csv|
+      csv << %[id name price]
+      @meals.each do |meal|
+        csv << [meal.id, meal.name, meal.price]
+      end
+  end
+end
