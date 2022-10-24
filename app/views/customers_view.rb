521d72d177d87aae19e1278431f4b4d82f72c6dc
Ling Fung Chan <justinlfchan@gmail.com>
2022-10-24 22:05:50 +0100

    created customers view

---
+++ app/views/customers_view.rb
@@ -0,0 +1,13 @@
+class CustomersView
+  def ask_user_for(stuff)
+    puts("What is the #{stuff}?")
+    print '> '
+    gets.chomp.capitalize
+  end
+
+  def display(customers)
+    customers.each_with_index do |customer, index|
+      puts "#{index + 1} - #{customer.name} at #{customer.address}"
+    end
+  end
+end
