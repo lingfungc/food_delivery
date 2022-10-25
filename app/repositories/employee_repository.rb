a128b8c742b152a8f495bfe4b8d66f80397064f5
Ling Fung Chan <justinlfchan@gmail.com>
2022-10-25 20:31:09 +0100

    created and tested employee repository all_riders find find_by_username methods

--- app/repositories/employee_repository.rb
+++ app/repositories/employee_repository.rb
@@ -8,7 +8,17 @@ class EmployeeRepository
     load_csv if File.exist?(@csv_file)
   end
 
+  def all_riders
+    @employees.select { |employee| employee.role == 'rider' }
+  end
 
+  def find(id)
+    @employees.find { |employee| employee.id == id }
+  end
+
+  def find_by_username(username)
+    @employees.find { |employee| employee.username == username }
+  end
 
   private
 
@@ -20,3 +30,16 @@ class EmployeeRepository
     end
   end
 end
+
+# EMPLOYEE_CSV_FILE = File.join(__dir__, '../data/employee.csv')
+EMPLOYEE_CSV_FILE = '/Users/lingfungc/code/lingfungc/food_delivery/data/employee.csv'
+
+employee_repo = EmployeeRepository.new(EMPLOYEE_CSV_FILE)
+
+p employee_repo
+
+p employee_repo.all_riders
+
+p employee_repo.find(2)
+
+p employee_repo.find_by_username('Justin')
